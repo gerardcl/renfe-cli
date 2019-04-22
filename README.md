98b56c3e4b8e4f21918a44f50b18c8ab4b8d47d6
gerardcl <gerardcl@gmail.com>
2019-04-22 22:02:18 +0200

    search funtionality ready

--- README.md
+++ README.md
@@ -8,15 +8,23 @@ Usage: renfe.py [options]
 
 Options:
   -h, --help            show this help message and exit
-  -y YEAR, --year=YEAR
-  -m MONTH, --month=MONTH
-  -d DAY, --day=DAY
-  -o ORIGIN, --origin=ORIGIN
-  -t TO, --to=TO
+  -y YEAR, --year=YEAR          -- timetable's year
+  -m MONTH, --month=MONTH       -- timetable's month
+  -d DAY, --day=DAY             -- timetable's day
+  -o ORIGIN, --origin=ORIGIN    -- timetable's origin station ID
+  -t TO, --to=TO                -- timetable's destination stations ID
+  -s SEARCH, --search=SEARCH    -- search for IDs related to train station names
 ```
-**Note**: defaults to `today`, from `Sils` to `Barcelona`
-___
-TODO
-----
-- ./renfe -u: create/update list of stations with IDs
-- ./renfe -s: search/guess right station name
+**Note 1**: if not searching for IDs, timetable defaults to `today`, from `Sils` to `Barcelona`
+
+**Note 2**: when using search functionality, it will provide you with the IDs (to use as an origin or destiation train station) of the stations that are similar to the input text to search. Example:
+
+```
+./renfe.py -s sil
+Today is: 2019-04-22
+Searching stations like: sil
+SANESTEVODOSIL 22003
+SANPEDRODOSIL 22004
+SILLA 64200
+SILS 79202
+```
--- renfe.py
+++ renfe.py
@@ -6,26 +6,81 @@ warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 import pandas as pd
 
+# defaults
 stations = {'SILS':'79202','BCN':'BARCE'}
 today = date.today()
 
+# opt parser
 p = optparse.OptionParser()
 p.add_option('--year', '-y', default=today.year)
 p.add_option('--month', '-m', default=today.month)
 p.add_option('--day', '-d', default=today.day)
 p.add_option('--origin', '-o', default="SILS")
 p.add_option('--to', '-t', default="BCN")
+p.add_option('--search', '-s', default='')
 
 options, arguments = p.parse_args()
 
 print("Today is: {}".format(today))
-print("Searching timetable for date: {}-{}-{}".format(options.year, options.month, options.day))
-print("From {} to {}".format(options.origin, options.to))
 
-url = 'http://horarios.renfe.com/HIRRenfeWeb/buscar.do?O={}&D={}&ID=s&AF={}&MF={}&DF={}'.format(stations.get(options.origin, '79202'), stations.get(options.to, 'BARCE'), options.year, options.month, options.day)
+# print timetable for given origin and to stations for a given date
+if options.search == '':
+  print("Searching timetable for date: {}-{}-{}".format(options.year, options.month, options.day))
+  print("From {} to {}".format(options.origin, options.to))
 
-tables = pd.read_html(url) # Returns list of all tables on page
-timetable = tables[4] # Select table of interest
-timetable = timetable.drop(timetable.columns[[4, 5, 6]], axis=1)
+  urlTimetable = 'http://horarios.renfe.com/HIRRenfeWeb/buscar.do?O={}&D={}&ID=s&AF={}&MF={}&DF={}'.format(stations.get(options.origin, '79202'), stations.get(options.to, 'BARCE'), options.year, options.month, options.day)
+  urlStations = 'http://horarios.renfe.com/HIRRenfeWeb/estaciones.do?&ID=s&icid=VTodaslasEstaciones'
 
-print(timetable)
+  tables = pd.read_html(urlTimetable) # Returns list of all tables on page
+  timetable = tables[4] # Select table of interest
+  timetable = timetable.drop(timetable.columns[[4, 5, 6]], axis=1)
+
+  print(timetable)
+
+# search into list of Station names and its Renfe identifiers
+else:
+  print("Searching stations like: {}".format(options.search))
+  import urllib.request
+
+  urlStations = 'http://horarios.renfe.com/HIRRenfeWeb/estaciones.do?&ID=s&icid=VTodaslasEstaciones'
+
+  web = urllib.request.urlopen(urlStations)
+  content =  web.read().decode(web.headers.get_content_charset())
+
+  import re
+
+  data = re.subn(r'<(html).*?</\1>(?s)', '', content)[0]
+
+  from html.parser import HTMLParser
+
+  class MyHTMLParser(HTMLParser):
+    def __init__(self):
+      HTMLParser.__init__(self)
+      self.recording = 0
+      self.data = []
+      self.links = []
+
+    def handle_starttag(self, tag, attrs):
+      if tag == 'a':
+        attrs = dict(attrs)
+      if tag == 'a' and attrs.get('class') == 'linkgrise':
+        self.links.append(attrs['href'].split('\'')[1])
+        self.recording = 1
+
+    def handle_endtag(self, tag):
+      if tag == 'a':
+        self.recording = 0
+
+    def handle_data(self, data):
+      if self.recording and re.sub('\s+', '', data) != '' and len(data) > 1:
+        self.data.append(re.sub('\s+', '', data))
+
+  parser = MyHTMLParser()
+  parser.feed(data)
+  parser.close()
+
+  stationsWithID = dict(zip(parser.data, parser.links))
+
+  for key in stationsWithID:
+    if options.search.lower() in key.lower():
+        print(key, stationsWithID.get(key))
