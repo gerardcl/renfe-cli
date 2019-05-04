e125239f6acc337411514445e2562a0baca44477
Gerard Castillo <gerard.castillo@boehringer-ingelheim.com>
2019-05-05 01:13:38 +0200

    reverting changes, creating new release 1.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='renfe-cli',
-    version='1.1.1',
+    version='1.1.2',
     description='Get faster RENFE Spanish Trains timetables in your terminal',
     long_description=read_md('README.md'),
     keywords='Get faster RENFE Spanish Trains timetables terminal',
