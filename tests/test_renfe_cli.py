e142c5ccc255a8847f75598f5566adced8d5f482
gerardcl <gerardcl@gmail.com>
2019-05-06 19:45:45 +0200

    adding tests

--- tests/test_renfe_cli.py
+++ tests/test_renfe_cli.py
@@ -7,29 +7,37 @@ from renfe.cli import main
 prog = 'renfe-cli'
 today = date.today()
 
+
 def test_commands():
-    for cmd, exp in test_input:
-        def check_command(cmd, exp):
-            base = [prog]
-            args = cmd.split(' ')
-            if not args == ['']: # need this for 'just `renfe-cli`' command test
-                base.extend(args)
-            sys.argv = base
-            if type(exp) == type(Exception):
-                assert_raises(exp, main)
-            else:
-                output = main()
-                assert output == exp
-        check_command.description = "command: %s %s" % (prog, cmd)
-        yield check_command, cmd, exp
+  for cmd, exp in test_input:
+    def check_command(cmd, exp):
+      base = [prog]
+      args = cmd.split(' ')
+      if not args == ['']:  # need this for 'just `renfe-cli`' command test
+        base.extend(args)
+      sys.argv = base
+      if type(exp) == type(Exception):
+        assert_raises(exp, main)
+      else:
+        output = main()
+        assert output == exp
+    check_command.description = "command: %s %s" % (prog, cmd)
+    yield check_command, cmd, exp
+
 
 test_input = (
-    # default
-    ('', None),
+  # default
+  ('', None),
 
-    # same as default
-    ('-y {} -m {} -d {} -o 79202 -t BARCE'.format(today.year, today.month, today.day), None),
+  # same as default
+  ('-y {} -m {} -d {} -o 79202 -t BARCE'.format(today.year, today.month, today.day), None),
 
-    # search
-    ('-s sil', None), ('-s barc', None)
+  # search ok
+  ('-s sil', None), ('-s barc', None),
+
+  # search nook
+  ('-s 123', None),
+
+  # wrong inputs
+  ('-d notanumber', SystemExit), ('-o BARCE -t BARCE', SystemExit), ('-m 30', SystemExit)
 )
