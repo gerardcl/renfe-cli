a2751d76d82aa9fe7ea55e349d755f78f5f0e899
Gerard Castillo <gerard.castillo@boehringer-ingelheim.com>
2020-04-17 22:59:26 +0200

    cleaning sonarqube code smells and follow more PEPs

--- src/renfe/cli.py
+++ src/renfe/cli.py
@@ -1,61 +1,79 @@
 #!/usr/bin/env python
-import sys
 import optparse
 import logging
 import colorama
 from datetime import date
+
 from renfe.interface import get_time_table, search_stations_ids
 from renfe.utils import RenfeException, ConfigurationMgmt
 
+
 def main():
-  # defaults
-  config = ConfigurationMgmt()
-  stations = config.get_config()
-  today = date.today()
+    # defaults
+    config = ConfigurationMgmt()
+    stations = config.get_config()
+    today = date.today()
 
-  # opt parser
-  p = optparse.OptionParser()
-  p.add_option('--year', '-y', default=today.year, help='year selected to get the timetable from')
-  p.add_option('--month', '-m', default=today.month, help='month of the year to get the timetable from')
-  p.add_option('--day', '-d', default=today.day, help='day of the month to get the timetable from')
-  p.add_option('--origin', '-o', default=stations.get('origin', '79202'), help='from/origin ID of the train station. Use flag \'-s <possible station name>\' in order to search for IDs')
-  p.add_option('--to', '-t', default=stations.get('to', 'BARCE'), help='to/destination ID of the train station. Use flag \'-s <possible station name>\' in order to search for IDs')
-  p.add_option('--search', '-s', default='', help='you need to get the stations IDs, searching by names; in order to apply right inputs for origins and/or destinations')
-  p.add_option('--logging-level', '-l', help='logging level defaults to warning and possible values are: debug, info, warning, error and critical')
-  p.add_option('--logging-file', '-f', help='logging file name is required if you want to submit an issue with more information')
-  p.add_option('--update-config', '-u', default=False, action='store_true', dest='update_config', help='change your origin and destination stations to defaults when loading this flag')
-  options, arguments = p.parse_args()
+    # opt parser
+    p = optparse.OptionParser()
+    p.add_option('--year', '-y', default=today.year, help='year selected to get the timetable from')
+    p.add_option('--month', '-m', default=today.month, help='month of the year to get the timetable from')
+    p.add_option('--day', '-d', default=today.day, help='day of the month to get the timetable from')
+    p.add_option('--origin', '-o', default=stations.get('origin', '79202'),
+                 help='from/origin ID of the train station. Use flag '
+                      '\'-s <possible station name>\' in order to search for IDs')
+    p.add_option('--to', '-t', default=stations.get('to', 'BARCE'),
+                 help='to/destination ID of the train station. Use flag '
+                      '\'-s <possible station name>\' in order to search for IDs')
+    p.add_option('--search', '-s', default='',
+                 help='you need to get the stations IDs, searching by names; '
+                      'in order to apply right inputs for origins and/or destinations')
+    p.add_option('--logging-level', '-l',
+                 help='logging level defaults to warning and possible values are:'
+                      ' debug, info, warning, error and critical')
+    p.add_option('--logging-file', '-f',
+                 help='logging file name is required if you want to submit an issue with more information')
+    p.add_option('--update-config', '-u', default=False, action='store_true', dest='update_config',
+                 help='change your origin and destination stations to defaults when loading this flag')
+    options, arguments = p.parse_args()
 
-  # check and configure input parameters
-  try:
-    config.configs_and_checks(options)
-    if options.update_config:
-      config.set_config({'origin': options.origin, 'to': options.to})
-  except RenfeException as err:
-    logging.error(err)
-    logging.error("Error configuring.. Check your inputs and enable debug. If the problem persists, create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
-    exit(1)
-
-  colorama.init(autoreset=True)
-  print(colorama.Fore.GREEN + "Today is: {}".format(today) + colorama.Fore.RESET)
-
-  # print timetable for given origin and to stations for a given date
-  if options.search == '':
+    # check and configure input parameters
     try:
-      get_time_table(options.origin, options.to, options.year, options.month, options.day)
+        config.configs_and_checks(options)
+        if options.update_config:
+            config.set_config({'origin': options.origin, 'to': options.to})
     except RenfeException as err:
-      logging.error(err)
-      logging.error("No timetables found... Check your inputs and enable debug. If the problem persists, create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
-      exit(1)
+        logging.error(err)
+        logging.error(
+            "Error configuring.. Check your inputs and enable debug. If the problem persists,"
+            " create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
+        exit(1)
 
-  # search into list of Station names and its Renfe identifiers
-  else:
-    try:
-      search_stations_ids(options.search)
-    except RenfeException as err:
-      logging.error(err)
-      logging.error("Error searching stations IDs... Check your inputs and enable debug. If the problem persists, create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
-      exit(1)
+    colorama.init(autoreset=True)
+    print(colorama.Fore.GREEN + "Today is: {}".format(today) + colorama.Fore.RESET)
+
+    # print timetable for given origin and to stations for a given date
+    if options.search == '':
+        try:
+            get_time_table(options.origin, options.to, options.year, options.month, options.day)
+        except RenfeException as err:
+            logging.error(err)
+            logging.error(
+                "No timetables found... Check your inputs and enable debug. If the problem persists,"
+                " create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
+            exit(1)
+
+    # search into list of Station names and its Renfe identifiers
+    else:
+        try:
+            search_stations_ids(options.search)
+        except RenfeException as err:
+            logging.error(err)
+            logging.error(
+                "Error searching stations IDs... Check your inputs and enable debug. If the problem persists,"
+                " create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
+            exit(1)
+
 
 if __name__ == '__main__':
-  main()
+    main()
--- src/renfe/interface.py
+++ src/renfe/interface.py
@@ -1,74 +1,77 @@
 import sys
 import colorama
 import logging
-import ssl
 import urllib.request
 import certifi
 
 from renfe.utils import RenfeException, RenfeHTMLParser
 
+
 def get_time_table(origin, to, year, month, day):
-  try:
-    import warnings
-    warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-    warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-    import pandas as pd
+    try:
+        import warnings
+        warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+        warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+        import pandas as pd
 
-    # nice input values for month and day
-    if len(str(month)) == 1:
-      month = '0' + str(month)
-    if len(str(day)) == 1:
-      day = '0' + str(day)
+        # nice input values for month and day
+        if len(str(month)) == 1:
+            month = '0' + str(month)
+        if len(str(day)) == 1:
+            day = '0' + str(day)
 
-    print(colorama.Fore.GREEN + "Searching timetable for date: {}-{}-{}".format(year, month, day) + colorama.Fore.RESET)
-    print(colorama.Fore.GREEN + "From {} to {}".format(origin, to) + colorama.Fore.RESET)
+        print(colorama.Fore.GREEN + "Searching timetable for date: {}-{}-{}".format(year, month,
+                                                                                    day) + colorama.Fore.RESET)
+        print(colorama.Fore.GREEN + "From {} to {}".format(origin, to) + colorama.Fore.RESET)
 
-    url_timetable = 'https://horarios.renfe.com/HIRRenfeWeb/buscar.do?O={}&D={}&ID=s&AF={}&MF={}&DF={}'.format(origin, to, year, month, day)
+        url_timetable = 'https://horarios.renfe.com/HIRRenfeWeb/buscar.do?O={}&D={}&ID=s&AF={}&MF={}&DF={}'.format(
+            origin, to, year, month, day)
 
-    logging.debug(url_timetable)
+        logging.debug(url_timetable)
 
-    # get timetable
-    web = urllib.request.urlopen(url_timetable, cafile=certifi.where())
-    tables = pd.read_html(web) # Returns list of all tables on page
-    timetable = tables[4] # Select table of interest
-    timetable = timetable.drop(timetable.columns[[4, 5, 6]], axis=1) # Remove not required columns
-    # show timetable
-    print(colorama.Fore.GREEN + "=================== TIMETABLE ====================" + colorama.Fore.RESET)
-    print(timetable)
-    print(colorama.Fore.GREEN + "==================================================" + colorama.Fore.RESET)
-  except:
-    logging.debug(sys.exc_info())
-    raise RenfeException("An error occurred while getting timetable")
+        # get timetable
+        web = urllib.request.urlopen(url_timetable, cafile=certifi.where())
+        tables = pd.read_html(web)  # Returns list of all tables on page
+        timetable = tables[4]  # Select table of interest
+        timetable = timetable.drop(timetable.columns[[4, 5, 6]], axis=1)  # Remove not required columns
+        # show timetable
+        print(colorama.Fore.GREEN + "=================== TIMETABLE ====================" + colorama.Fore.RESET)
+        print(timetable)
+        print(colorama.Fore.GREEN + "==================================================" + colorama.Fore.RESET)
+    except:
+        logging.debug(sys.exc_info())
+        raise RenfeException("An error occurred while getting timetable")
+
 
 def search_stations_ids(search):
-  try:
-    import re
-    print(colorama.Fore.GREEN + "Searching stations like: {}".format(search) + colorama.Fore.RESET)
+    try:
+        import re
+        print(colorama.Fore.GREEN + "Searching stations like: {}".format(search) + colorama.Fore.RESET)
 
-    url_stations = 'https://horarios.renfe.com/HIRRenfeWeb/estaciones.do?&ID=s&icid=VTodaslasEstaciones'
+        url_stations = 'https://horarios.renfe.com/HIRRenfeWeb/estaciones.do?&ID=s&icid=VTodaslasEstaciones'
 
-    logging.debug(url_stations)
+        logging.debug(url_stations)
 
-    found_any = False
+        found_any = False
 
-    web = urllib.request.urlopen(url_stations, cafile=certifi.where())
-    content =  web.read().decode(web.headers.get_content_charset())
-    data = re.subn('', content,r'<(html).*?</\1>(?s)')[0]
+        web = urllib.request.urlopen(url_stations, cafile=certifi.where())
+        content = web.read().decode(web.headers.get_content_charset())
+        data = re.subn('', content, r'<(html).*?</\1>(?s)')[0]
 
-    parser = RenfeHTMLParser()
-    parser.feed(data)
-    parser.close()
+        parser = RenfeHTMLParser()
+        parser.feed(data)
+        parser.close()
 
-    stations_with_id = dict(zip(parser.data, parser.links))
+        stations_with_id = dict(zip(parser.data, parser.links))
 
-    # show findings
-    for key in stations_with_id:
-      if search.lower() in key.lower():
-        found_any = True
-        print(key +": "+ stations_with_id.get(key))
+        # show findings
+        for key in stations_with_id:
+            if search.lower() in key.lower():
+                found_any = True
+                print(key + ": " + stations_with_id.get(key))
 
-    if not found_any:
-      print("Sorry, no stations found for input value: {}".format(search))
-  except:
-    logging.debug(sys.exc_info())
-    raise RenfeException("An error occurred while searching for stations IDs")
+        if not found_any:
+            print("Sorry, no stations found for input value: {}".format(search))
+    except Exception:
+        logging.debug(sys.exc_info())
+        raise RenfeException("An error occurred while searching for stations IDs")
